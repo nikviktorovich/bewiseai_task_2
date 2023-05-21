066e30e9e72a0b27fa30f601b3e182db19f60909
lnfecteDru <nikitasmn98@gmail.com>
2023-05-21 15:47:33 +0300

    Added creating db tables script

---
+++ db/sql/create_tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR UNIQUE,
+    access_token UUID UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS audio (
+    uuid UUID PRIMARY KEY,
+    user_id INT NOT NULL,
+    audio_filepath VARCHAR NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
