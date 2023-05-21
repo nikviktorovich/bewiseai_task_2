c34866057cc635ac8fe94c55f46087b0774cc987
lnfecteDru <nikitasmn98@gmail.com>
2023-05-21 14:36:19 +0300

    Added orm mappers

--- src/audio_converter/database/mappers.py
+++ src/audio_converter/database/mappers.py
@@ -1,8 +1,20 @@
 import sqlalchemy.orm
 
+import audio_converter.modules.audio.database.models
+import audio_converter.modules.audio.domain.models
+import audio_converter.modules.user.database.models
+import audio_converter.modules.user.domain.models
+
 
 def start_mappers() -> None:
     registry = sqlalchemy.orm.registry()
 
-    # TODO: Add mappers
-    ...
+    registry.map_imperatively(
+        audio_converter.modules.audio.domain.models.Audio,
+        audio_converter.modules.audio.database.models.Audio,
+    )
+
+    registry.map_imperatively(
+        audio_converter.modules.user.domain.models.User,
+        audio_converter.modules.user.database.models.User,
+    )
